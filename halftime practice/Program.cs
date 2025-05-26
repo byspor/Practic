926b6b805667f2d45ddff63e1271dcec7e0d3115
byspor <slokvami@mail.ru>
2025-05-26 08:57:13 +0300

    пробую серилизовать и десериализовать списки в json

--- halftime practice/Program.cs
+++ halftime practice/Program.cs
@@ -1,16 +1,25 @@
 ﻿using static System.Console;
 using System.Text.Json;
 
-Product product = new Product {Name = "Яблоко", Price = 50, InStock = true };
+//Product product = new Product {Name = "Яблоко", Price = 50, InStock = true };
+List<Product> products = new List<Product>
+{
+    new Product { Name = "Яблоко", Price = 50, InStock = true},
+    new Product { Name = "Молоко", Price = 70, InStock = false},
+    new Product { Name = "Хлеб", Price = 30, InStock = true}
+};
 
 var options = new JsonSerializerOptions {WriteIndented = true };
-string json = JsonSerializer.Serialize(product, options);
+string json = JsonSerializer.Serialize(products, options);
 await File.WriteAllTextAsync("product.json", json);
 
 string loadedJson = await File.ReadAllTextAsync("product.json");
-Product loadedProduct = JsonSerializer.Deserialize<Product>(loadedJson);
+List<Product> loadedProduct = JsonSerializer.Deserialize<List<Product>>(loadedJson);
 
-WriteLine($"Продукт - {loadedProduct.Name, -10} | Цена - {loadedProduct.Price, -3} | Наличие - {(loadedProduct.InStock ? "Да" : "Нет"), -3}|");
+foreach (var product in loadedProduct)
+{
+    WriteLine($"Продукт - {product.Name,-10} | Цена - {product.Price,-3} | Наличие - {(product.InStock ? "Да" : "Нет"),-3}|");
+}
 public class Product
 {
     public string Name { get; set; }
