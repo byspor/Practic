0b72e9bb5fc7b8a99753d151b4629b5a9088785d
byspor <slokvami@mail.ru>
2025-05-26 08:57:13 +0300

    попробовал десериализовать json с вложенными списками пробую серилизовать и десериализовать списки в json и вложенные объекты

--- halftime practice/Program.cs
+++ halftime practice/Program.cs
@@ -1,19 +1,76 @@
 ﻿using static System.Console;
 using System.Text.Json;
 
-Product product = new Product {Name = "Яблоко", Price = 50, InStock = true };
+List<Product> products = new List<Product>
+{
+    new Product { Name = "Яблоко", Price = 50, InStock = true},
+    new Product { Name = "Молоко", Price = 70, InStock = false},
+    new Product { Name = "Хлеб", Price = 30, InStock = true}
+};
 
-var options = new JsonSerializerOptions {WriteIndented = true };
-string json = JsonSerializer.Serialize(product, options);
-await File.WriteAllTextAsync("product.json", json);
+User user = new User
+{
+    Name = "Иван",
+    Age = 25,
+    Profile = new Profile 
+    {  
+        City = "Москва",
+        Email = "ivan@example.com"
+    }
+};
 
-string loadedJson = await File.ReadAllTextAsync("product.json");
-Product loadedProduct = JsonSerializer.Deserialize<Product>(loadedJson);
+var options = new JsonSerializerOptions { WriteIndented = true };
+string json = JsonSerializer.Serialize(user, options);
+await File.WriteAllTextAsync("user.json", json);
 
-WriteLine($"Продукт - {loadedProduct.Name, -10} | Цена - {loadedProduct.Price, -3} | Наличие - {(loadedProduct.InStock ? "Да" : "Нет"), -3}|");
+if (!File.Exists("user.json"))
+{
+    WriteLine("Файл не найден");
+    return;
+}
+
+try
+{
+    string loadedJson = await File.ReadAllTextAsync("user.json");
+    User loadedUsers = JsonSerializer.Deserialize<User>(loadedJson);
+
+    WriteLine($"Имя: {loadedUsers.Name}, Возраст: {loadedUsers.Age}");
+
+    WriteLine($"Город: {loadedUsers.Profile.City}");
+    WriteLine($"Email: {loadedUsers.Profile.Email}");
+
+    //foreach (var product in loadedProduct)
+    //{
+    //    WriteLine($"Продукт - {product.Name,-10} | Цена - {product.Price,-3} | Наличие - {(product.InStock ? "Да" : "Нет"),-3}|");
+    //}
+}
+
+catch (JsonException)
+{
+    WriteLine("Ошибка: файл содержит некорректный JSON");
+}
+
+catch (Exception ex)
+{
+    WriteLine($"Ошибка: {ex.Message}");
+}
+
 public class Product
 {
     public string Name { get; set; }
     public int Price { get; set; }
     public bool InStock { get; set; }
+}
+
+public class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Profile Profile { get; set; }
+}
+
+public class Profile
+{
+    public string City { get; set; }
+    public string Email { get; set; }
 }
