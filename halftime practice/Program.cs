3932a295acbced1f201957b35968de727c731e7f
byspor <slokvami@mail.ru>
2025-05-27 09:30:25 +0300

    попробовал создать пользовательский конвертер

--- halftime practice/Program.cs
+++ halftime practice/Program.cs
@@ -1,18 +1,20 @@
 ﻿using static System.Console;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+
+
 User user = new User
 {
     Name = "Иван",
     Age = 25,
-    Profile = new Profile 
-    {  
-        City = "Москва",
-        Email = "ivan@example.com"
-    }
+    Favourites = new List<string> { "яблоко", "груша", "апельсин" }
+
 };
 
+
 var options = new JsonSerializerOptions { WriteIndented = true };
+options.Converters.Add(new StringToListConverter());
+
 string json = JsonSerializer.Serialize(user, options);
 await File.WriteAllTextAsync("user.json", json);
 
@@ -25,12 +27,16 @@ if (!File.Exists("user.json"))
 try
 {
     string loadedJson = await File.ReadAllTextAsync("user.json");
-    User loadedUsers = JsonSerializer.Deserialize<User>(loadedJson);
+    User loadedUsers = JsonSerializer.Deserialize<User>(loadedJson, options);
 
     WriteLine($"Имя: {loadedUsers.Name}, Возраст: {loadedUsers.Age}");
 
-    WriteLine($"Город: {loadedUsers.Profile.City}");
-    WriteLine($"Email: {loadedUsers.Profile.Email}");
+    WriteLine($"Любимые товары: ");
+
+    foreach (var item in loadedUsers.Favourites)
+    {
+        WriteLine($"- {item}");
+    }
 
 }
 
@@ -44,6 +50,24 @@ catch (Exception ex)
     WriteLine($"Ошибка: {ex.Message}");
 }
 
+public class StringToListConverter : JsonConverter<List<string>>
+{
+    public override List<string> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            string value = reader.GetString();
+            return value?.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        return JsonSerializer.Deserialize<List<string>>(ref reader, options);
+    }
+
+    public override void Write(Utf8JsonWriter writer, List<string> value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(string.Join(",", value));
+    }
+}
 public class Product
 {
     public string Name { get; set; }
@@ -53,17 +77,21 @@ public class Product
 
 public class User
 {
-    [JsonPropertyName("username")]    
+    [JsonPropertyName("username")]
     public string Name { get; set; }
+
     [JsonPropertyName("user_age")]
     public int Age { get; set; }
-    public Profile Profile { get; set; }
+    //public Profile Profile { get; set; }
+
+    public List<string> Favourites { get; set; }
 }
 
 public class Profile
 {
     [JsonPropertyName("location")]
     public string City { get; set; }
+
     [JsonPropertyName("contact_email")]
     public string Email { get; set; }
 }
